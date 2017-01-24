0dce5863158523d10b5af238202fb8b4dd2433c5
Angelo Agatino Nicolosi <angelonicolosi@yanchware.com>
2017-01-24 21:25:45 +0100

    Added navigation to other views

--- src/views/article.vue
+++ src/views/article.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    Article
-  </div>
+    <div id="article">
+        Article
+    </div>
 </template>
+
+<script>
+export default {
+  name: 'article',
+  components: {
+  },
+
+  mounted () {
+    let el = window.$('#toggle')
+    if (!window.sliding) {
+      window.sliding = true
+      el.hide('slide', {direction: 'right'}, 100, function () {
+        el.show('slide', {direction: 'right'}, 1000, function () {
+          window.sliding = false
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/portfolio.vue
+++ src/views/portfolio.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    Portfolio
-  </div>
+    <div id="portfolio">
+        Portfolio
+    </div>
 </template>
+
+<script>
+export default {
+  name: 'portfolio',
+  components: {
+  },
+
+  mounted () {
+    let el = window.$('#toggle')
+    if (!window.sliding) {
+      window.sliding = true
+      el.hide('slide', {direction: 'right'}, 100, function () {
+        el.show('slide', {direction: 'right'}, 1000, function () {
+          window.sliding = false
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
